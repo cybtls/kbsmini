edea3356f8b03a1ac1d9294d0f5af2d7800ed368
cyb <838182202@qq.com>
2021-08-25 22:04:24 +0800

    自定义tabbar

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,11 +2,11 @@ const app = getApp()
 
 Page({
   data: {
-    titleBarOptions: {
-      title: 'options设置的标题',
-      center: true,
-      'class': 'custom-class'
-    },
+    // titleBarOptions: {
+    //   title: 'options设置的标题',
+    //   center: true,
+    //   'class': 'custom-class'
+    // },
     tabBarOptions: {
       active: 0,
       list: [{
@@ -14,13 +14,13 @@ Page({
           page: 'MyBill',
           text: '账单'
         },
-        {
-          icon: 'icon-add',
-          page: 'home',
-          text: '',
-          action: true,
-          actionClass: 'bg-green'
-        },
+        // {
+        //   icon: 'icon-add',
+        //   page: 'home',
+        //   text: '',
+        //   action: true,
+        //   actionClass: 'bg-green'
+        // },
         {
           icon: 'icon-peoplefill',
           page: 'MySetting',
